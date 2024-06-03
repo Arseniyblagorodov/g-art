56e830baafcb3f9bf569cc4934aaabe32533f771
NepoGostu <andruavanshuls@gmail.com>
2024-06-03 18:13:33 +0300

    fix: filter;

--- src/js/_tabFilter.js
+++ src/js/_tabFilter.js
@@ -1,20 +1,26 @@
-const tabsItems = document.querySelectorAll('.cards__tabs .button');
-console.log("tabsItems" , tabsItems);
-const cardsItems = document.querySelectorAll('.cards .card__item');
-console.log("cardsItems ", cardsItems);
-const showAllButton = document.getElementById('show_all');
-console.log("showAllButton", showAllButton);
+document.addEventListener('DOMContentLoaded', () => {
 
-tabsItems.forEach(tabItem => {
-    tabItem.addEventListener('click', () => {
-        console.log("click")
-        tabsItems.forEach(item => {
-            // console.log(tabItem.textContent)
+    const tabsItems = document.querySelectorAll('.cards__tabs .button');
+    console.log("tabsItems", tabsItems);
+    const cardsItems = document.querySelectorAll('.cards .card__item');
+    console.log("cardsItems ", cardsItems);
+    const showAllButton = document.getElementById('show_all');
+    console.log("showAllButton", showAllButton);
+
+    tabsItems.forEach(tabItem => {
+        tabItem.addEventListener('click', () => {
+            console.log("click")
+
             const filter = tabItem.textContent
-            const filteredTabs = Array.from(cardsItems).filter(item => {
-                item.textContent.includes(filter)
-            })
-            console.log("filteredTabs", filteredTabs);
+            console.log('filter', filter)
+            const filteredTabsEasy = Array.from(cardsItems).filter(block => block.textContent.includes(filter)); // короткая запись. Когда есть только одно выражение в теле функции, можно опустить фигурные скобки и слово return
+            const filteredTabsFull = Array.from(cardsItems).filter((block) => {
+                return block.textContent.includes(filter)
+            });// это полная запись
+            console.log("filteredTabsEasy", filteredTabsEasy);
+            console.log('filteredTabsFull', filteredTabsFull)
         })
     })
 })
+
+
